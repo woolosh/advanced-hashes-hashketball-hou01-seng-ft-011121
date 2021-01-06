f2dce266513b7cae1bab93f68f0d164ddcfe2601
Joshua Morales <joshuapmorales@gmail.com>
2021-01-06 07:09:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,12 +217,13 @@ def big_shoe_rebounds
       all_sizes << shoe_size(player[:player_name])
     end 
   end
-  
-  game_hash.each do |key, value|
-    value[:players].each do |player|
-      if player[:shoe] == largest_shoe_size
-        return player[:rebounds]
-      end
-    end
-  end    
+ 
+ return all_sizes 
+  # game_hash.each do |key, value|
+  #   value[:players].each do |player|
+  #     if player[:shoe] == largest_shoe_size
+  #       return player[:rebounds]
+  #     end
+  #   end
+  # end    
 end
